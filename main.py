b9de5bf819a0ed851fe51ec74bf2f0a03b48f66f
sudhir kumar patel <sudhir.kumar2020@vitbhopal.ac.in>
2022-12-23 00:20:56 +0530

    NEW CODE ADDED

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-from flask import Flask, render_template, request,session
+# NEW CODE
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 import json
 from datetime import datetime
 
 
-with open('config.json','r') as c:
-    params=json.load(c) ["params"]
-local_server=True
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+local_server = True
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
-
 
 
 # mail=Mail(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
 
-if(local_server):
+if (local_server):
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
 
 db = SQLAlchemy(app)
-
 
 
 class Contact(db.Model):
@@ -36,6 +35,7 @@ class Contact(db.Model):
     phone_num = db.Column(db.String(12), nullable=False)
     date = db.Column(db.String(12), nullable=True)
 
+
 class Post(db.Model):
 
     sno = db.Column(db.Integer, primary_key=True)
@@ -47,58 +47,56 @@ class Post(db.Model):
     date = db.Column(db.String(12), nullable=True)
 
 
-
 @app.route("/")
 def home():
-    posts = Post.query.filter_by().all() [0:params['number_of_post']]
-    return render_template('index.html',params=params,posts=posts)
+    posts = Post.query.filter_by().all()[0:params['number_of_post']]
+    return render_template('index.html', params=params, posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html',params=params)
+    return render_template('about.html', params=params)
+
 
 @app.route("/dashboard")
 def dashboard():
-    if ('user' in session and session['user']==params['admin_user']):
-        post=Post.querry.all()
+    if ('user' in session and session['user'] == params['admin_user']):
+        post = Post.querry.all()
         return render_template('dashboard.html')
 
-    if request.method=="POST":
-        #REDIRECT TO ADMIN PANEL
-        username=request.form.get('uname')
+    if request.method == "POST":
+        # REDIRECT TO ADMIN PANEL
+        username = request.form.get('uname')
         userpass = request.form.get('pass')
 
-        if(username==params['admin_user'] and userpass==params['admin_password'] ):
-               session['user']=username
-               return render_template('dashboard.html' ,param=params)
-
+        if (username == params['admin_user'] and userpass == params['admin_password']):
+            session['user'] = username
+            return render_template('dashboard.html', param=params)
 
     return render_template("login.html", params=params)
 
-@app.route("/contact", methods = ['GET', 'POST'])
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    if(request.method=='POST'):
+    if (request.method == 'POST'):
         '''Add entry to the database'''
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
         message = request.form.get('message')
-        entry = Contact(name=name, email = email,phone_num = phone, mes = message, date= datetime.now() )
+        entry = Contact(name=name, email=email, phone_num=phone,
+                        mes=message, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
 
-    return render_template('contact.html',params=params)
+    return render_template('contact.html', params=params)
 
-@app.route("/post/<string:post_slug>",methods=['GET'])
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
 def post_route(post_slug):
-    post=Post.query.filter_by(slug=post_slug).first()
+    post = Post.query.filter_by(slug=post_slug).first()
 
-
-
-    return render_template('post.html',params=params, post=post)
+    return render_template('post.html', params=params, post=post)
 
 
 app.run(debug=True)
-
-
